5c1f9eb02367eac8b07eba96583b98f6f9082291
Firas Yahyaoui <firas.yahyaoui@imt-atlantique.net>
2022-12-07 00:37:45 +0100

    Fixed images issue + Added main navigation

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    //public string sceneName;
     void Start()
     {
         
@@ -17,7 +18,11 @@ public class MainMenu : MonoBehaviour
         
     }
 
-    public void LoadChallengesScene() {
-        SceneManager.LoadScene(1);
+    public void LoadChallengesScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadScenarioScene(string sceneName) {
+	SceneManager.LoadScene(sceneName);
     }
 }
