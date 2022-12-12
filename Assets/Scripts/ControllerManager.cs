0282f004340ee65ca9115bd1cb3e66bfe1b55a96
BrenoHA <breno.ha@gmail.com>
2022-12-12 23:59:13 +0100

    Add: Notification messages to tutorial mode

--- Assets/Scripts/ControllerManager.cs
+++ Assets/Scripts/ControllerManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,12 +9,18 @@ public class ControllerManager : MonoBehaviour
 
     private List<GameObject> children;
     private int modeActive;
+    private bool isTeleportationActive;
+    public GameObject Equipments;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        NotificationManager.Instance.SetNewNotification("You arrived at the lab, check if you can walk and move your hands properly", 8f);
+        StartCoroutine(ExecuteTPMessageAfterTime(8));
         switchMode.action.performed += ctx => PerformSwitchMode();
-
+        isTeleportationActive = false;
         children = new List<GameObject>();
         Transform[] allChildren = GetComponentsInChildren<Transform>();
         foreach (Transform child in transform)
@@ -32,6 +39,12 @@ public class ControllerManager : MonoBehaviour
         children[modeActive].SetActive(true);
     }
 
+    private void Update()
+    {
+        
+
+    }
+
     private void PerformSwitchMode()
     {
         foreach (GameObject child in children)
@@ -39,6 +52,23 @@ public class ControllerManager : MonoBehaviour
             child.SetActive(false);
         }
         modeActive = (modeActive + 1) % children.Count;
+        isTeleportationActive = (modeActive == 1) ? true : false;
+        if (isTeleportationActive) {
+            ExecuteGrabMessageAfterTime(4);
+        }
         children[modeActive].SetActive(true);
+    }
+
+
+    IEnumerator ExecuteGrabMessageAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        NotificationManager.Instance.SetNewNotification("Try to grab an empty glass", 18f);
+    }
+
+    IEnumerator ExecuteTPMessageAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        NotificationManager.Instance.SetNewNotification("Teleport to the lab table and the to the computer table", 8f);
     }
 }
