a97175dc4c5f5d618a0446f6705b4159060643c1
BrenoHA <breno.ha@gmail.com>
2022-12-13 11:25:13 +0100

    Fix: Notification messages in the tutorial mode

--- Assets/Scripts/ControllerManager.cs
+++ Assets/Scripts/ControllerManager.cs
@@ -10,15 +10,19 @@ public class ControllerManager : MonoBehaviour
     private List<GameObject> children;
     private int modeActive;
     private bool isTeleportationActive;
-    public GameObject Equipments;
 
+    private List<GameObject> equipment;
+    public GameObject selectedEquipment;
+    private List<Vector3> initialPositions;
+    private bool positionsChanged = false;
+    private bool startUpdate = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        NotificationManager.Instance.SetNewNotification("You arrived at the lab, check if you can walk and move your hands properly", 8f);
-        StartCoroutine(ExecuteTPMessageAfterTime(8));
+        NotificationManager.Instance.SetNewNotification("You arrived at the lab, check if you can walk and move your hands properly", 12f);
+        StartCoroutine(ExecuteTPMessageAfterTime(12));
         switchMode.action.performed += ctx => PerformSwitchMode();
         isTeleportationActive = false;
         children = new List<GameObject>();
@@ -31,6 +35,20 @@ public class ControllerManager : MonoBehaviour
             }
         }
 
+        equipment = new List<GameObject>();
+        initialPositions = new List<Vector3>();
+        Transform[] allEquipment = selectedEquipment.GetComponentsInChildren<Transform>();
+        foreach (Transform child in selectedEquipment.transform)
+        {
+            if (child.CompareTag("LabMaterial"))
+            {
+                equipment.Add(child.gameObject);
+                initialPositions.Add(child.gameObject.transform.position);
+            }
+        }
+
+        
+
         foreach (GameObject child in children)
         {
             child.SetActive(false);
@@ -41,7 +59,17 @@ public class ControllerManager : MonoBehaviour
 
     private void Update()
     {
-        
+        if(!positionsChanged && startUpdate) { 
+            for (int i = 0; i < equipment.Count; i++)
+            {
+                if (!equipment[i].gameObject.transform.position.Equals(initialPositions[i]))
+                {
+                    positionsChanged = true;
+                    StartCoroutine(ExecuteTurnItMessageAfterTime(12));
+                    StartCoroutine(ExecuteFinalMessageAfterTime(22));
+                }
+            }
+        }
 
     }
 
@@ -54,21 +82,32 @@ public class ControllerManager : MonoBehaviour
         modeActive = (modeActive + 1) % children.Count;
         isTeleportationActive = (modeActive == 1) ? true : false;
         if (isTeleportationActive) {
-            ExecuteGrabMessageAfterTime(4);
+            StartCoroutine(ExecuteGrabMessageAfterTime(8));
         }
         children[modeActive].SetActive(true);
     }
+    IEnumerator ExecuteTurnItMessageAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        NotificationManager.Instance.SetNewNotification("Great! Grab another glass and turn one into the other", 12f);
+    }
 
+    IEnumerator ExecuteFinalMessageAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        NotificationManager.Instance.SetNewNotification("Congratulations, you finished the tutorial!", 28f);
+    }
 
     IEnumerator ExecuteGrabMessageAfterTime(float time)
     {
         yield return new WaitForSeconds(time);
-        NotificationManager.Instance.SetNewNotification("Try to grab an empty glass", 18f);
+        NotificationManager.Instance.SetNewNotification("Try to grab a glass using the grip button", 14f);
+        startUpdate = true;
     }
 
     IEnumerator ExecuteTPMessageAfterTime(float time)
     {
         yield return new WaitForSeconds(time);
-        NotificationManager.Instance.SetNewNotification("Teleport to the lab table and the to the computer table", 8f);
+        NotificationManager.Instance.SetNewNotification("Press Y (Left hand) and then try to teleport to the lab table", 12f);
     }
 }
